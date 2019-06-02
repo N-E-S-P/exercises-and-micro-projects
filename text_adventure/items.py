ab16f0489db5e35e63f46fcc852fa44c9566a00f
Sebastian B <43120511+N-E-S-P@users.noreply.github.com>
2019-06-02 11:50:19 +0200

    Add files via upload

--- text_adventure/items.py
+++ text_adventure/items.py
@@ -1,12 +1,13 @@
 # the base class for all items
 class Item:
-    def __init__(self, name, description, value):
+    def __init__(self, name, description, value, amount):
         self.name = name
         self.description = description
         self.value = value
+        self.amount = amount
 
     def __str__(self):
-        return "{}\n=====\n{}\nValue: {}\n".format(self.name, self.description, self.value)
+        return "{}\n=====\n{}\nValue: {}\n".format(self.name, self.description, self.value, self.amount)
 
 
 class GoldCoin(Item):
@@ -14,15 +15,40 @@ class GoldCoin(Item):
         self.amount = amount
         super(GoldCoin, self).__init__(name="Gold Coin",
                                        description="A single unit of currency.",
-                                       value=self.amount)
+                                       value=self.amount,
+                                       amount=self.amount)
 
 
 class Ruby(Item):
-    def __init__(self, amount):
-        self.amount = amount
+    def __init__(self):
         super(Ruby, self).__init__(name="Ruby",
                                    description="A precious ruby, as scarlet as human blood.",
-                                   value=100)
+                                   value=100,
+                                   amount=self.amount)
+
+
+class Key(Item):
+    def __init__(self):
+        super(Key, self).__init__(name="Key",
+                                  description="A small, rusty key.",
+                                  value=0,
+                                  amount=self.amount)
+
+
+class Arrow(Item):
+    def __init__(self):
+        super(Arrow, self).__init__(name="Arrow",
+                                    description="A sharp arrow, to be used with a bow.",
+                                    value=1,
+                                    amount=self.amount)
+
+
+class Rope(Item):
+    def __init__(self):
+        super(Rope, self).__init__(name="Rope",
+                                   description="A hemp rope, useful for overcoming obstacles.",
+                                   value=15,
+                                   amount=self.amount)
 
 
 # WEAPONS
@@ -43,4 +69,10 @@ class Dagger(Weapon):
                                      damage=5)
 
 
+class Bow(Weapon):
+    def __init__(self):
+        super(Bow, self).__init__(name="Bow",
+                                  description="A wooden bow, good for hunting.",
+                                  value=10,
+                                  damage=5)
 
