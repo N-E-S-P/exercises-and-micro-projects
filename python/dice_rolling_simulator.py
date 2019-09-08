a8110caa8eb561feea9cbc849571d7df8963ddfe
Sebastian B <43120511+N-E-S-P@users.noreply.github.com>
2019-09-08 23:46:39 +0200

    Add files via upload

---
+++ python/dice_rolling_simulator.py
@@ -0,0 +1,14 @@
+import random
+
+
+def dice_rolling_simulator():
+    print("Result:", random.randint(1, 6))
+
+    try_again = input("Wanna try again? ")
+
+    if try_again.lower() == "y" or try_again.lower() == "yes":
+        return dice_rolling_simulator()
+    elif try_again.lower() == "n" or try_again.lower() == "no":
+        return "Sad dice. :("
+    else:
+        return "I did not understand. Retry."
