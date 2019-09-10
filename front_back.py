923003ab83c27753bfbf78b0f060d802859793a7
Sebastian B <43120511+N-E-S-P@users.noreply.github.com>
2019-09-10 11:11:52 +0200

    Delete front_back.py

--- front_back.py
+++
@@ -1,9 +0,0 @@
-# Given a string, return a new string where the first and last chars have been exchanged.
-
-
-def front_back(s):
-    if len(s) > 1:
-        s = ''.join([s[-1], s[1:-1], s[0]])
-        return s
-    else:
-        return s
