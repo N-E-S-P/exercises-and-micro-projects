e3c2e12806224d9726bff8e32600e7a5066de2af
Sebastian B <43120511+N-E-S-P@users.noreply.github.com>
2019-09-22 14:03:30 +0200

    Add files via upload

--- python/every_term_counter.py
+++ python/every_term_counter.py
@@ -2,10 +2,17 @@
 
 def every_term_counter(file):
     counter = {}
-    words = open(file, "r").read().split()
-    for word in words:
+    punctuation = "!""£$%&/()=?^[]*+-@#§.:,;_{}`~''"
+    words = open(file, "r").read().lower()
+    for marker in punctuation:
+        words = words.replace(marker, "")
+    for word in words.split():
         if word in counter:
             counter[word] += 1
         else:
             counter[word] = 1
-    return counter
+    outfile = open("frequency_distribution.txt", "w+")
+    for word, frequency in counter.items():
+        outfile.write(word + ": " + str(frequency) + "\n")
+    outfile = outfile.close()
+    return outfile
